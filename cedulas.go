ca840c87a423b3581a81922455345abee0c1162f
Rudá Torquato <rudatorquato@gmail.com>
2020-06-14 22:51:24 -0300

    limpando codigo

--- cedulas.go
+++ cedulas.go
@@ -13,27 +13,20 @@ func main() {
 
 	if cedulas > 0 {
 		cem = cedulas / 100
-		p("Notas de 100:")
-		p(cem)
 	}
 	if cem > 0 {
 		result1 = cedulas - (cem * 100)
-		//p("result1")
-		//p(result1)
 		if result1 > 0 {
 			cinquenta = result1 / 50
-			p("Notas de 50:")
-			p(cinquenta)
 		}
 	}
 	if cinquenta > 0 {
 		result2 = cedulas - (cem * 100) - (cinquenta * 50)
-		//p("result2")
-		//p(result2)
 		if result1 > 0 {
 			cinco = result2 / 5
-			p("Notas de 5:")
-			p(cinco)
-		}
+		}		
 	}
+	p("Notas de 100:", cem)
+		p("Notas de 50:", cinquenta)
+		p("Notas de 5:",cinco)
 }
